574baf6d15fca454e386c174bddf266ccd7685b9
Shuai Wang <swang28@ncsu.edu>
2019-04-24 18:10:25 -0400

    hash table test

--- app/controllers/github_metrics_controller.rb
+++ app/controllers/github_metrics_controller.rb
@@ -34,7 +34,7 @@ class GithubMetricsController < ApplicationController
       hyperlink_data["repository_name"] = submission_hyperlink_tokens.pop
       hyperlink_data["owner_name"] = submission_hyperlink_tokens.pop
       github_data = get_pull_request_details(hyperlink_data)
-      @head_refs[hyperlink_data["pull_request_number"]] = {
+      @github_metrics_data[:head_refs[hyperlink_data["pull_request_number"]]] = {
           head_commit: github_data["data"]["repository"]["pullRequest"]["headRefOid"],
           owner: hyperlink_data["owner_name"],
           repository: hyperlink_data["repository_name"]
@@ -76,7 +76,7 @@ class GithubMetricsController < ApplicationController
   end
 
   def retrieve_check_run_statuses
-    @head_refs.each do |pull_number, pr_object|
+    @github_metrics_data[:head_refs].each do |pull_number, pr_object|
       @check_statuses[pull_number] = get_statuses_for_pull_request(pr_object)
     end
   end
@@ -89,7 +89,16 @@ class GithubMetricsController < ApplicationController
       return
     end
 
-    @head_refs = {}
+    @github_metrics_data = {
+        :head_refs =>{}
+    }
+
+   # @github_metrics_data = {
+   #     :head_refs =>{}, :parsed_data => {}, :authors => {}, :dates => {}, :total_additions => 0, :total_deletions => 0,
+   #     :total_commits => 0, :total_files_changed => 0, :merge_status => {}, :check_statuses => {}
+   #}
+
+   # @head_refs = {}
     @parsed_data = {}
     @authors = {}
     @dates = {}
